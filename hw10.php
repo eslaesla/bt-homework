a0fd1dc66c367271279e7f8f303e99d1cc67b271
eslaesla <eslaesla@hotmail.com>
2022-04-29 11:22:56 +0200

    prvi zadatak

--- hw10.php
+++ hw10.php
@@ -1,4 +1,9 @@
 <?php 
+$a=200;
+$b=80;
+echo  "Zbir je ". " " .$a+$b . "<br>";
+echo "Razlika je " . " " .$a-$b. " <br>";
+echo "Proizvod je " . " " .$a*$b. "<br> ";
+echo "Kolicnik je " . " " .$a/$b. " <br>";
 
-asdasd
 ?>
