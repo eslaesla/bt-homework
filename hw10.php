abe4eda8e85848b12b4f61f05888321c40727bd0
eslaesla <eslaesla@hotmail.com>
2022-05-02 16:44:11 +0200

    domaci

--- hw10.php
+++ hw10.php
@@ -1,9 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<H2>Prvi zadatak</H2>
 <?php 
+
+//Prvi zadatak
 $a=200;
 $b=80;
 echo  "Zbir je ". " " .$a+$b . "<br>";
 echo "Razlika je " . " " .$a-$b. " <br>";
 echo "Proizvod je " . " " .$a*$b. "<br> ";
 echo "Kolicnik je " . " " .$a/$b. " <br>";
+?>
+<br>
+<br>
+<br>
+
+<h2>Drugi zadatak</h2>
+<?php
+//Drugi zadatak
+$dan=array ("Nedelja","Ponedeljak","Utorak","Sreda","Cetvrtak","Petak","Subota");
+$a=3;
+echo "Danas je" . " " .$dan[$a];
+?>
+<br><br><br>
+
+<h3>Treci zadatak</h3>
+
+<?php
+$a=2;
+$b=4;
+$c=7;
+echo "Zbir brojeva $a i $b i $c je" . " " .$a+$b+$c ;
+echo "<br>";
+echo max($a,$b,$c);
 
 ?>
+
+</html>
